94e5a217c52f27bdf785ad7f5bb4a7ca43062b0e
alenagrigoryeva <alenagrigoryeva02@gmail.com>
2023-09-09 18:02:05 +0900

    first commit

---
+++ plugins/plugins.js
@@ -0,0 +1,60 @@
+/*jslint node: true */
+
+'use strict';
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+	});
+	$('.scrollup').click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+});
+
+$('a[href^="#"').on('click', function () {
+
+    var href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 500,
+		easing: "linear"
+	});
+
+    return false;
+});
+
+new WOW().init();
+
+$('#owl-carousel-feedback').owlCarousel({
+    loop: true,
+    items: 1,
+    dots: true,
+    autoHeight: true
+});
+
+$('#owl-carousel-gallery').owlCarousel({
+    margin: 10,
+    center: true,
+    loop: true,
+    dots: false,
+    autoWidth: true,
+    items: 5
+});
+
+$('#owl-carousel-gallery').on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY > 0) {
+        $('#owl-carousel-gallery').trigger('next.owl');
+    } else {
+        $('#owl-carousel-gallery').trigger('prev.owl');
+    }
+    e.preventDefault();
+});
